337cd8a0aabea2fdae37ab5079f7645a2a13d57b
yeoneeeeee <kny050406@naver.com>
2024-12-05 21:29:32 +0900

    Add files via upload

---
+++ 2743번.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int n, m;
+int arr1[101][101], arr2[101][101];
+int main() {
+	scanf_s("%d %d", &n, &m);
+	for (int i = 0; i < n; i++)
+		for (int k = 0; k < m; k++)
+			scanf_s("%d", &arr1[i][k]);
+	for (int i = 0; i < n; i++)
+		for (int k = 0; k < m; k++)
+			scanf_s("%d", &arr2[i][k]);
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < m; k++)
+			printf("%d ", arr1[i][k] + arr2[i][k]);
+		printf("\n");
+	}
+	return 0;
+}
